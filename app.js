c95a4f9e436e696f760b366039bc7058f7d30340
Sebastian Sanabria <zettoseb27@gmail.com>
2026-04-11 14:19:16 -0500

    eamos urlencoded y json

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+// 1 Invocamos Exprress
 const express = require("express");
 const app = express();
 
+// 2 Seteamos urlencoded y json para que el servidor pueda interpretar los datos que le llegan
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// 3 Invocamos a dotenv para cargar las variables de entorno
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
