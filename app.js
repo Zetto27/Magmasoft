826abba6a649968c12a205d7f4175440a2007293
Sebastian Sanabria <zettoseb27@gmail.com>
2026-04-11 23:17:10 -0500

    registro funcional

--- app.js
+++ app.js
@@ -43,6 +43,64 @@ app.get("/registro", (req, res) => {
 });
 
 // 10 Ruta para registrar usuarios
+app.post("/register", async (req, res) => {
+  console.log(req.body);
+  const user = req.body.user;
+  const rol = req.body.rol;
+  const document = req.body.document;
+  const email = req.body.email;
+  const celular = req.body.celular;
+  const pass = req.body.pass;
+
+  let passwordHash = await bcryptjs.hash(pass, 8);
+
+  connection.query(
+    "INSERT INTO users SET ?",
+    {
+      user,
+      rol,
+      document,
+      email,
+      celular,
+      pass: passwordHash,
+    },
+    (error, results) => {
+      if (error) {
+        if (error.code === "ER_DUP_ENTRY") {
+          return res.render("register", {
+            alert: true,
+            alertTitle: "Dato duplicado",
+            alertMessage: "Ya existe un registro con ese correo, documento o celular.",
+            alertIcon: "warning",
+            showConfirmButton: true,
+            timer: false,
+            ruta: "/registro",
+          });
+        }
+
+        return res.render("register", {
+          alert: true,
+          alertTitle: "Error",
+          alertMessage: "Ocurrió un error al registrar",
+          alertIcon: "error",
+          showConfirmButton: true,
+          timer: false,
+          ruta: "/registro",
+        });
+      }
+
+      return res.render("register", {
+        alert: true,
+        alertTitle: "Registro exitoso",
+        alertMessage: "Usuario creado correctamente",
+        alertIcon: "success",
+        showConfirmButton: false,
+        timer: 1500,
+        ruta: "/login",
+      });
+    },
+  );
+});
 
 app.listen(3000, () => {
   console.log("Server is running in http://localhost:3000");
